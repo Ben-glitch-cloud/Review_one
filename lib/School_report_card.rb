7f4324980090fee31bffc9f57875e36aadd50e3d
Benedict Lawrence <BTLawrence44@gmail.com>
2021-02-08 16:29:09 +0000

    counting the grades

--- lib/School_report_card.rb
+++ lib/School_report_card.rb
@@ -1,5 +1,9 @@
-def SchoolReport(grades)
-    p grades
+def SchoolReport(grades) 
+    grade_level = grades.split(",").map { |word| word.delete(" ").capitalize}.uniq
+
+    grades.split(",").each do |words|
+       p words.delete(" ").capitalize.count(grade_level)
+    end
 end 
 
 SchoolReport("Green, green, Amber, Red, Red, Red")
--- spec/School_report_card_spec.rb
+++ spec/School_report_card_spec.rb
@@ -1,2 +1,7 @@
 require 'School_report_card' 
 
+describe SchoolReport do
+    it 'should return a report card' do
+        expect(SchoolReport).to eq("Green: 3")
+    end
+end
