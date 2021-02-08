25bb0d883f341b3451e1df5be509aff01c7178ff
Benedict Lawrence <BTLawrence44@gmail.com>
2021-02-08 16:45:27 +0000

    last commit

--- lib/School_report_card.rb
+++ lib/School_report_card.rb
@@ -2,10 +2,18 @@ def SchoolReport(grades)
     grade_level = grades.split(",").map { |word| word.delete(" ").capitalize}.uniq 
 
     grade_level_two = grades.split(",").map { |word| word.delete(" ").capitalize} 
+    
+    array = []
 
-    grade_level.each do |words|  
-        puts "#{words}: #{grade_level_two.count(words)}"
+    grade_level.each do |words|   
+        if words == "Green" || words == "Amber" || words == "Red"
+            puts "#{words}: #{grade_level_two.count(words)}"  
+        else  
+            array << words 
+            p array
+            #puts "#{words}: #{grade_level_two.count(words)}" + "uncounted words: #{array.length}"
+        end 
     end 
 end 
 
-SchoolReport("Green, green, Amber, Red, Red, Red")
+SchoolReport("Green, green, Amber, Red, Red, Red, purple")
