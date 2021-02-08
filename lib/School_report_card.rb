b9fc747cbfc51178ac5af2c443fe4735bf6bba32
Benedict Lawrence <BTLawrence44@gmail.com>
2021-02-08 16:34:56 +0000

    number of grades counted

--- lib/School_report_card.rb
+++ lib/School_report_card.rb
@@ -1,9 +1,11 @@
 def SchoolReport(grades) 
-    grade_level = grades.split(",").map { |word| word.delete(" ").capitalize}.uniq
+    grade_level = grades.split(",").map { |word| word.delete(" ").capitalize}.uniq 
 
-    grades.split(",").each do |words|
-       p words.delete(" ").capitalize.count(grade_level)
-    end
+    grade_level_two = grades.split(",").map { |word| word.delete(" ").capitalize} 
+
+    grade_level.each do |words|  
+        puts "#{words}: #{grade_level_two.count(words)}"
+    end 
 end 
 
 SchoolReport("Green, green, Amber, Red, Red, Red")
